af9b182e404b5c8cd7084f9ec81af62e68af60b8
Abimael Santos <abymaelsantos@gmail.com>
2023-12-15 14:19:24 -0300

    Criando projeto em Java de Fatorial

---
+++ src/Fatorial/Fatorial.java
@@ -0,0 +1,15 @@
+
+package Fatorial;
+
+public class Fatorial {
+
+    public static void main(String[] args) {
+        //Fatorial
+        int fat = 1;
+        int valor = 6;
+        for(int i=1; i <= valor; i++){
+            fat*=i;
+            System.out.println(fat);
+        } 
+    } 
+}
